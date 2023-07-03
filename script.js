8f830d728c86dc854ecf6ca14ee2326e2fab2fc7
lavanyaa121 <121863812+lavanyaa121@users.noreply.github.com>
2023-07-03 20:22:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -36,6 +36,18 @@ function rot13(encodedStr) {
   // Only change code below this line
 
   return; //return decodedArr
+};
+function rot13(encodedStr) {
+  let decodedArr = []; // Your Result goes here
+  // Only change code below this line
+	for(let i=0; i<str.lenght:i++){
+		if(lookup[str[i]]==undefined){
+			decodedArr.push(str[i])
+ 
+		else decodedArr.push(lookup[str[i]])
+	}
+ 
+  return; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
